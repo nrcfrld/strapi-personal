68b74ce098889edf46a36aeebdb286ac56e60127
Enrico Feraldo Kalengkongang <nrcfrld@gmail.com>
2022-11-29 15:31:37 +0700

    first commit

---
+++ config/middlewares.js
@@ -0,0 +1,24 @@
+module.exports = ({ env }) => [
+  "strapi::errors",
+  {
+    name: "strapi::security",
+    config: {
+      contentSecurityPolicy: {
+        directives: {
+          "default-src": ["'self'"],
+          "img-src": ["'self'", "data:", "blob:", env("SUPABASE_API_URL"), "*"],
+          "script-src": ["'self'", "'unsafe-inline'", env("SUPABASE_API_URL")],
+        },
+      },
+    },
+  },
+  "strapi::security",
+  "strapi::cors",
+  "strapi::poweredBy",
+  "strapi::logger",
+  "strapi::query",
+  "strapi::body",
+  "strapi::session",
+  "strapi::favicon",
+  "strapi::public",
+];
